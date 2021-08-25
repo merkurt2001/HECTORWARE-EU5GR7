b5a176e7eea2c3c3f4e89df3a012f1aa692f7bde
RobertoCDS <81467045+RobertoCDS@users.noreply.github.com>
2021-08-25 10:41:22 +0100

    Changed space

--- src/test/java/hectorware/pages/SaitLoginPage.java
+++ src/test/java/hectorware/pages/SaitLoginPage.java
@@ -1,6 +1,6 @@
 package hectorware.pages;
 
-import hectorware.utilities.Driver;
+import hectorware.utilities.Driver ;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
