525916bf92a6584c262b7206ba4ab8f1e77eb4de
RobertoCDS <81467045+RobertoCDS@users.noreply.github.com>
2021-08-28 20:49:06 +0100

    Added right click method and cleaned unused methods.

--- src/test/java/com/hectorware/utilities/WebTools.java
+++ src/test/java/com/hectorware/utilities/WebTools.java
@@ -1,14 +1,8 @@
 package com.hectorware.utilities;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class WebTools {
 
@@ -19,49 +13,12 @@ public class WebTools {
             e.printStackTrace();
         }
     }
-    public static WebElement waitForClickablility(By locator, int timeout) {
-        WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
-        return wait.until(ExpectedConditions.elementToBeClickable(locator));
-    }
-    public static void clickWithWait(By by, int attempts) {
-        int counter = 0;
-        //click on element as many as you specified in attempts parameter
-        while (counter < attempts) {
-            try {
-                //selenium must look for element again
-                clickWithJS(Driver.get().findElement(by));
-                //if click is successful - then break
-                break;
-            } catch (WebDriverException e) {
-                //if click failed
-                //print exception
-                //print attempt
-                e.printStackTrace();
-                ++counter;
-                //wait for 1 second, and try to click again
-                waitFor(1);
-            }
-        }
-    }
+    public static void rightClick(WebElement element) {
+        new Actions(com.hectorware.utilities.Driver.get()).contextClick(element).perform();
 
-    public static void clickWithJS(WebElement element) {
-        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].scrollIntoView(true);", element);
-        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].click();", element);
     }
-
-    public static String getScreenshot(String name) throws IOException {
-        // name the screenshot with the current date time to avoid duplicate name
-        String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-        // TakesScreenshot ---> interface from selenium which takes screenshots
-        TakesScreenshot ts = (TakesScreenshot) Driver.get();
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        // full path to the screenshot location
-        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
-        File finalDestination = new File(target);
-        // save the screenshot to the path given
-        FileUtils.copyFile(source, finalDestination);
-        return target;
+    public static void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) com.hectorware.utilities.Driver.get()).executeScript("arguments[0].scrollIntoView(true);", element);
     }
-
 
 }
